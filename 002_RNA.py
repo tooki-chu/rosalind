50704c908a0dc56f7a865852c0ae989ffe0f39ae
Tooki <42063660+tooki-chu@users.noreply.github.com>
2020-08-02 00:03:00 +0800

    Added solution to Transcribing DNA into RNA

---
+++ 002_RNA.py
@@ -0,0 +1,22 @@
+'''
+ROSALIND - Bioinformatics Stronghold
+Problem: Transcribing DNA into RNA
+'''
+
+def transcribe(s):
+    """
+    Returns transcribed RNA string of a given DNA string
+    """
+
+    return s.replace('T', 'U')
+
+
+def main():
+    with open('datasets/rosalind_rna.txt', 'r') as f:
+        dna = f.read()
+
+    # print the counts
+    print(transcribe(dna))
+
+if __name__ == '__main__':
+    main()
